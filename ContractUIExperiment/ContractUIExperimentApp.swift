c046107f66cc2924d245145ddb827b6c1b9198ad
Alex Basson <alexbasson@gmail.com>
2021-02-20 17:44:01 -0500

    Initial Commit

---
+++ ContractUIExperiment/ContractUIExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContractUIExperimentApp.swift
+//  ContractUIExperiment
+//
+//  Created by Alex on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ContractUIExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
